427d655add990adf1b20b32e23dd095c3f427dbf
Kalai0561 <112849591+Kalai0561@users.noreply.github.com>
2022-09-07 19:59:41 +0530

    Add files via upload

---
+++ reverse hill pattern in c++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5,i,j;
+
+	for(i=i;i<=n;i++){
+		for(j=1;j<=i;j++){
+			cout<<"  ";
+		}
+		for(j=i;j<n;j++){
+			cout<<"* ";
+		}
+		for(j=i;j<=n;j++){
+			cout<<"* ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
